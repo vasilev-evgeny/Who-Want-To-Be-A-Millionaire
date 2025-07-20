b950a5edb81927a4e400c102b21ee594a84bc29d
vasiliev-evgeny <vasiliev.creativnoemilo99@gmail.com>
2025-07-20 20:08:16 +0300

    Initial Commit

---
+++ Team 4 Group Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Team 4 Group Challenge
+//
+//  Created by Евгений Васильев on 20.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
