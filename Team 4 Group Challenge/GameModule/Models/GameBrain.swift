2c5a8a88c68e2b5ba3c0066c3b0735c209350309
vasiliev-evgeny <vasiliev.creativnoemilo99@gmail.com>
2025-07-21 16:11:53 +0300

    GameBrain
    добавил именно 15 вопросов и функцию создания массива вопросов

--- Team 4 Group Challenge/GameModule/Models/GameBrain.swift
+++ Team 4 Group Challenge/GameModule/Models/GameBrain.swift
@@ -7,48 +7,101 @@
 import UIKit
 
 class GameBrain {
+    
     var easy: [Question] = [
         Question(
-            question: "В каком городе находится самая большая в мире система метро по количеству станций?",
-            answers: ["Токио", "Москва", "Нью-Йорк", "Шанхай"],
-            correctAnswer: "Нью-Йорк"
-        ),
-        Question(
-            question: "Какой город называют 'Городом любви' из-за его романтической атмосферы?",
+            question: "Какой город называют 'Городом любви'?",
             answers: ["Венеция", "Прага", "Париж", "Рим"],
             correctAnswer: "Париж"
         ),
         Question(
-            question: "В каком городе находится самый высокий небоскрёб в мире — Бурдж-Халифа?",
-            answers: ["Дубай", "Шанхай", "Токио", "Нью-Йорк"],
-            correctAnswer: "Дубай"
+            question: "Где находится статуя Христа-Искупителя?",
+            answers: ["Буэнос-Айрес", "Рио-де-Жанейро", "Лима", "Сантьяго"],
+            correctAnswer: "Рио-де-Жанейро"
+        ),
+        Question(
+            question: "В каком городе находится Колизей?",
+            answers: ["Афины", "Стамбул", "Рим", "Каир"],
+            correctAnswer: "Рим"
+        ),
+        Question(
+            question: "Какой город построен на 118 островах?",
+            answers: ["Амстердам", "Стокгольм", "Венеция", "Копенгаген"],
+            correctAnswer: "Венеция"
+        ),
+        Question(
+            question: "Где находится Сиднейский оперный театр?",
+            answers: ["Мельбурн", "Окленд", "Сидней", "Брисбен"],
+            correctAnswer: "Сидней"
         )
     ]
     
     var medium: [Question] = [
         Question(
-            question: "Какой город называют 'Вечным городом' из-за его древней истории?",
-            answers: ["Афины", "Рим", "Каир", "Иерусалим"],
-            correctAnswer: "Рим"
+            question: "Какой город называют 'Большим яблоком'?",
+            answers: ["Чикаго", "Лос-Анджелес", "Нью-Йорк", "Торонто"],
+            correctAnswer: "Нью-Йорк"
         ),
         Question(
-            question: "В каком городе находится самый большой в мире парк развлечений — Disney World?",
-            answers: ["Лос-Анджелес", "Токио", "Орландо", "Париж"],
-            correctAnswer: "Орландо"
+            question: "Где находится древний город Петра?",
+            answers: ["Египет", "Иордания", "Сирия", "Ирак"],
+            correctAnswer: "Иордания"
+        ),
+        Question(
+            question: "Какой город является самым густонаселённым в мире?",
+            answers: ["Дели", "Шанхай", "Токио", "Сан-Паулу"],
+            correctAnswer: "Токио"
+        ),
+        Question(
+            question: "Где находится район Небесных мостов?",
+            answers: ["Сингапур", "Куала-Лумпур", "Дубай", "Гонконг"],
+            correctAnswer: "Сингапур"
+        ),
+        Question(
+            question: "Какой город называют 'Городом ветров'?",
+            answers: ["Чикаго", "Бостон", "Торонто", "Детройт"],
+            correctAnswer: "Чикаго"
         )
     ]
     
     var hard: [Question] = [
         Question(
-            question: "Какой город является самым северным миллионником в мире?",
+            question: "Какой город является самым северным миллионником?",
             answers: ["Осло", "Хельсинки", "Санкт-Петербург", "Стокгольм"],
             correctAnswer: "Санкт-Петербург"
         ),
         Question(
-            question: "В каком городе находится самая узкая улица в мире — Шпройерхофштрассе (ширина 31 см)?",
+            question: "Где находится самая узкая улица (31 см)?",
             answers: ["Прага", "Ротенбург-на-Таубере", "Венеция", "Амстердам"],
             correctAnswer: "Ротенбург-на-Таубере"
+        ),
+        Question(
+            question: "Какой город построен на месте древнего озера?",
+            answers: ["Мехико", "Каир", "Дели", "Бангкок"],
+            correctAnswer: "Мехико"
+        ),
+        Question(
+            question: "Где находится самый длинный морской мост?",
+            answers: ["Китай", "Япония", "США", "ОАЭ"],
+            correctAnswer: "Китай"
+        ),
+        Question(
+            question: "Какой город имеет форму самолёта при виде сверху?",
+            answers: ["Бразилиа", "Канберра", "Астана", "Исламабад"],
+            correctAnswer: "Бразилиа"
         )
     ]
     
+    private var newGameQuestion = [Question]()
+
+    func createQuestionArray() {
+        if newGameQuestion.isEmpty {
+            easy.shuffle()
+            medium.shuffle()
+            hard.shuffle()
+            newGameQuestion.append(contentsOf: easy)
+            newGameQuestion.append(contentsOf: medium)
+            newGameQuestion.append(contentsOf: hard)
+        }
+    }
 }
