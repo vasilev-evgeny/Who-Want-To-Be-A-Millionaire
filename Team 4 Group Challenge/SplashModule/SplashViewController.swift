064c7af83ef5cb3e02207ef6d7f41307f7b21d74
mkaasvh <masha.kaverina.72@mail.ru>
2025-07-22 12:06:35 +0800

    feat: complete splash window

--- Team 4 Group Challenge/SplashModule/SplashViewController.swift
+++ Team 4 Group Challenge/SplashModule/SplashViewController.swift
@@ -14,23 +14,56 @@ class SplashViewController: UIViewController {
     }
     
     //MARK: - Create UI
+    let backgroundImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = UIImage(named: "Background")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+    }()
+    
+    let logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: "GameMillionaireIcon")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        imageView.layer.shadowColor = UIColor.black.cgColor
+        imageView.layer.shadowOpacity = 1
+        imageView.layer.shadowOffset = CGSize(width: 0, height: 0)
+        imageView.layer.shadowRadius = 67
+        imageView.layer.masksToBounds = false
+        
+        return imageView
+    }()
     
     //MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         setupViews()
         setConstraints()
     }
     
     private func setupViews() {
-        
+        view.addSubview(backgroundImageView)
+        view.addSubview(logoImageView)
     }
     
     //MARK: - setConstraints
     
     private func setConstraints() {
+        backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         
+        logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        logoImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        logoImageView.widthAnchor.constraint(equalToConstant: 267).isActive = true
+        logoImageView.heightAnchor.constraint(equalToConstant: 267).isActive = true
     }
 
 }
