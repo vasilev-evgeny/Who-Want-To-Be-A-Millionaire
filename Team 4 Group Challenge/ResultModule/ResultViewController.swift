50b873f51915f96ef33f36e83ddc86856e64cab8
mkaasvh <masha.kaverina.72@mail.ru>
2025-07-22 17:39:04 +0800

    feat: finished the second window - result window

--- Team 4 Group Challenge/ResultModule/ResultViewController.swift
+++ Team 4 Group Challenge/ResultModule/ResultViewController.swift
@@ -6,29 +6,125 @@
 //
 import UIKit
 
-class ResultViewController: UIViewController {
+class ResultViewController: BaseViewController {
 
     enum Constants {
-        
+
     }
     
+    private let startNewGameButton = Button()
+    private let goToMainScreenButton = Button()
+    
     //MARK: - Create UI
+    let subtitleLevel: UILabel = {
+        let label = UILabel()
+        label.textColor = .systemGray
+        label.textAlignment = .center
+        label.font = .boldSystemFont(ofSize: 16)
+        
+        return label
+    }()
+    
+    let coinLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = .boldSystemFont(ofSize: 24)
+        
+        return label
+    }()
+    
+    private lazy var labelWithCoinIconStack: UIStackView = {
+        let iconView = UIImageView()
+        iconView.image = UIImage(named: "CoinIcon")
+        iconView.translatesAutoresizingMaskIntoConstraints = false
+        iconView.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        iconView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        let stack = UIStackView(arrangedSubviews: [iconView, coinLabel])
+        stack.axis = .horizontal
+        stack.spacing = 2
+        stack.alignment = .center
+        iconView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return stack
+    }()
+    
+    private lazy var resultInfoStackView: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [subtitleLevel, labelWithCoinIconStack])
+        stack.axis = .vertical
+        stack.spacing = 4
+        stack.alignment = .center
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        
+        return stack
+    }()
+    
+    private lazy var buttonsStack: UIStackView = {
+        let stack = UIStackView(arrangedSubviews:  [startNewGameButton, goToMainScreenButton])
+        stack.axis = .vertical
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.spacing = 8
+        
+        [startNewGameButton, goToMainScreenButton].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.heightAnchor.constraint(equalToConstant: 75).isActive = true
+        }
+
+        return stack
+    }()
     
     //MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        titleLabel.text = "Game over!"
+        subtitleLevel.text = "Level 8"//потом можно вынести в переменные и передавать значения тут в зависимости от уровня пользователя и тд
+        coinLabel.text = "$15,000"
+        
         setupViews()
         setConstraints()
     }
     
     private func setupViews() {
+        view.addSubview(logoImageView)
+        view.addSubview(titleLabel)
+        view.addSubview(resultInfoStackView)
+        view.addSubview(buttonsStack)
         
+        startNewGameButton.titleText = "New game"
+        startNewGameButton.applyBackground(named: "YellowButton")
+        startNewGameButton.onTap = {
+            print("Start new game")
+        }
+        
+        goToMainScreenButton.titleText = "Main screen"
+        goToMainScreenButton.applyBackground(named: "BlueButton")
+        goToMainScreenButton.onTap = { 
+            print("go to main screen")
+        }
     }
     
     //MARK: - setConstraints
     
     private func setConstraints() {
+        logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 122).isActive = true
+        logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        logoImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        logoImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        
+        titleLabel.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 16).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32).isActive = true
+        titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32).isActive = true
+        
+        resultInfoStackView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 24).isActive = true
+        resultInfoStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        buttonsStack.topAnchor.constraint(equalTo: resultInfoStackView.bottomAnchor, constant: 124).isActive = true
+        buttonsStack.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        buttonsStack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true
+        buttonsStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
         
     }
 
--- Team 4 Group Challenge/SplashModule/SplashViewController.swift
+++ Team 4 Group Challenge/SplashModule/SplashViewController.swift
@@ -7,36 +7,14 @@
 
 import UIKit
 
-class SplashViewController: UIViewController {
+class SplashViewController: BaseViewController {
 
     enum Constants {
         
     }
     
     //MARK: - Create UI
-    let backgroundImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.contentMode = .scaleAspectFill
-        imageView.image = UIImage(named: "Background")
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        
-        return imageView
-    }()
-    
-    let logoImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.contentMode = .scaleAspectFit
-        imageView.image = UIImage(named: "GameMillionaireIcon")
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        
-        imageView.layer.shadowColor = UIColor.black.cgColor
-        imageView.layer.shadowOpacity = 1
-        imageView.layer.shadowOffset = CGSize(width: 0, height: 0)
-        imageView.layer.shadowRadius = 67
-        imageView.layer.masksToBounds = false
-        
-        return imageView
-    }()
+
     
     //MARK: - Lifecycle
     
@@ -48,18 +26,12 @@ class SplashViewController: UIViewController {
     }
     
     private func setupViews() {
-        view.addSubview(backgroundImageView)
         view.addSubview(logoImageView)
     }
     
     //MARK: - setConstraints
     
-    private func setConstraints() {
-        backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-        backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        
+    private func setConstraints() {        
         logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         logoImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         logoImageView.widthAnchor.constraint(equalToConstant: 267).isActive = true
