864b3a3d12461366382232b4e82dfd4e14710751
mkaasvh <masha.kaverina.72@mail.ru>
2025-07-26 15:53:23 +0800

    feat: complete questions

--- Team 4 Group Challenge/GameModule/Models/QuestionManager.swift
+++ Team 4 Group Challenge/GameModule/Models/QuestionManager.swift
@@ -1,81 +1,81 @@
-//
-//  QuestionManager.swift
-//  Team 4 Group Challenge
-//
-//  Created by Mariia on 24.07.2025.
-//
-
 import Foundation
-
-protocol QuestionManagerDelegate {
-    func didUpdateQuestion(question: [QuestionModal])
-}
 
 struct QuestionManager {
     let questionURL = "https://opentdb.com/api.php?amount=5&type=multiple"
     
-    var delegate: QuestionManagerDelegate?
-    
-    func fetchQuestions(_ difficulty: String) {
+    func fetchQuestions(_ difficulty: String, completion: @escaping ([Question]) -> Void) {
         let urlString = "\(questionURL)&difficulty=\(difficulty)"
-        
-        performRequest(urlString: urlString)
+        performRequest(urlString: urlString, completion: completion)
     }
     
-    func performRequest(urlString: String) {
+    private func performRequest(urlString: String, completion: @escaping ([Question]) -> Void) {
         if let url = URL(string: urlString) {
             let session = URLSession(configuration: .default)
             
             let task = session.dataTask(with: url) { (data, response, error) in
                 if error != nil {
                     print(error!)
+                    completion([])
                     return
                 }
                 
                 if let safeData = data {
                     if let question = self.parseJSON(questionData: safeData) {
-                        self.delegate?.didUpdateQuestion(question: question)
+                        completion(question)
+                    } else {
+                        completion([])
                     }
+                } else {
+                    completion([])
                 }
             }
             
             task.resume()
+        } else {
+            completion([])
         }
     }
     
-    func parseJSON(questionData: Data) -> [QuestionModal]? {
+    private func parseJSON(questionData: Data) -> [Question]? {
         let decoder = JSONDecoder()
         
         do {
             let decodedData = try decoder.decode(APIResponse.self, from: questionData)
-            
             let rawQuestions = decodedData.results
             let simplifiedQuestions = convertToSimplifiedGameQuestions(from: rawQuestions)
-            
             return simplifiedQuestions
-            
         } catch {
             print(error)
             return nil
         }
     }
     
-    func convertToSimplifiedGameQuestions(from rawQuestions: [QuestionData]) -> [QuestionModal] {
-        var simplifiedQuestionModal: [QuestionModal] = []
-          for item in rawQuestions {
-              let correct = item.correct_answer
-              let incorrect = item.incorrect_answers
-              let allAnswers = ([correct] + incorrect).shuffled()
+    private func convertToSimplifiedGameQuestions(from rawQuestions: [QuestionData]) -> [Question] {
+        var simplifiedQuestionModal: [Question] = []
+        for item in rawQuestions {
+            let correct = item.correct_answer.htmlDecoded
+            let incorrect = item.incorrect_answers.map { $0.htmlDecoded }
+            let allAnswers = ([correct] + incorrect).shuffled()
 
-              let question = QuestionModal(
-                  question: item.question,
-                  answers: allAnswers,
-                  correctAnswer: correct
-              )
-              
-              simplifiedQuestionModal.append(question)
-            }
-          
+            let question = Question(
+                question: item.question.htmlDecoded,
+                answers: allAnswers,
+                correctAnswer: correct
+            )
+
+            simplifiedQuestionModal.append(question)
+        }
         return simplifiedQuestionModal
+    }
+
+}
+
+extension String {
+    var htmlDecoded: String {
+        guard let data = self.data(using: .utf8) else { return self }
+        let options: [NSAttributedString.DocumentReadingOptionKey: Any] = [
+            .documentType: NSAttributedString.DocumentType.html
+        ]
+        return (try? NSAttributedString(data: data, options: options, documentAttributes: nil))?.string ?? self
     }
 }
